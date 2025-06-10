b418003c81aacbaa84217f40644e440d8b4f5e0b
Marcos <kisiohint@gmail.com>
2025-06-10 03:49:12 -0300

    Cambios modelo transaccion sigue fallando

--- tp-crypto/crypto-api/crypto-api/Models/Transaction.cs
+++ tp-crypto/crypto-api/crypto-api/Models/Transaction.cs
@@ -1,14 +1,24 @@
-﻿namespace crypto_api.Models
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+namespace crypto_api.Models
 {
     public class Transaction
     {
+        [Key]
        public int id { get; set; }
+        [NotNull,Required]
        public string action { get; set; }
+        [NotNull,Required]
        public string crypto_code { get; set; }
+        [NotNull, Required]
        public int client_id { get; set; }
        public Client? client_data { get; set; }
+        [NotNull]
        public float crypto_amount { get; set; }
+        [NotNull]
        public float money { get; set; }
+        [NotNull,Required]
        public DateTime datetime { get; set; }
     }
 }
--- tp-crypto/crypto-api/crypto-api/obj/Debug/net8.0/crypto-api.AssemblyInfo.cs
+++ tp-crypto/crypto-api/crypto-api/obj/Debug/net8.0/crypto-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crypto-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47f8268dc046ebd59735dd4b9c9f98605a0b527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f1fb88853629431eb3f83a1537cf7e6ba28180")]
 [assembly: System.Reflection.AssemblyProductAttribute("crypto-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crypto-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
